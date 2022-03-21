c99cffc65fa20e344780d5ecb91d2e78fc2e840a
limkim930 <1625753207lim@gmail.com>
2022-03-21 21:58:49 +0800

    add semicolons

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //引入express框架
 const express = require('express');
 //引入路径处理模块
-const path = require('path')
+const path = require('path');
 const formidable = require('formidable');
 const bodyParser = require('body-parser');
-const fs = require('fs')
-const thenFs = require('then-fs')
+const fs = require('fs');
+const thenFs = require('then-fs');
 const queryIp = require('node-ip2region').create();
 //阿里云
 const Core = require('@alicloud/pop-core');
@@ -15,7 +15,7 @@ const crypto = require('crypto');
 const app = express();
 
 //静态资源访问服务器功能
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json({ limit: '10mb' }));//防止图片过大报错
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));//防止图片过大报错
@@ -36,4 +36,4 @@ app.all("*", function (req, res, next) {
 app.listen(3000);
 
 //控制台提示输出
-console.log('服务器启动成功')
+console.log('服务器启动成功');
